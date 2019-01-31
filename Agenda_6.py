9ee89d2a6fb44b55ce8237655693585c8882729f
victor <v.a.lantaron@gmail.com>
2019-02-01 00:26:50 +0100

    exercise 6 complete

--- Agenda_6.py
+++ Agenda_6.py
@@ -1,16 +1,18 @@
-agenda = {}
-fin = "false"
+agenda = {"yo": "12345"}
+gfin = False
 
 
 def addNewContact():
+    print(agenda)
     name = input("inserta un nombre\n")
     tlf = input("inserta un telefono\n")
 
-    if ~agenda.hasKey('name'):
-        agenda[name] = tlf
-    else:
+    if agenda.get(name) is not None:
         print("el contacto ya existe\n")
         checkIfUpdate(name, tlf)
+    else:
+        agenda[name] = tlf
+        print("contacto " + agenda[name] + " añadido")
 
 
 def checkIfUpdate(name, tlf):
@@ -18,15 +20,17 @@ def checkIfUpdate(name, tlf):
         agenda[name] = tlf
     else:
         print("contacto no modificado")
+    return False
 
 
 def seeAgenda():
     for contacto in agenda:
-        print(contacto)
+        print(contacto + " - " + agenda[contacto])
+    return False
 
 
 def endProgram():
-   fin = "true"
+    return True
 
 
 def menu(opcion):
@@ -36,11 +40,11 @@ def menu(opcion):
         3: endProgram
     }
     ver = switch.get(int(opcion), lambda: "option not defined")
-    ver()
+    return ver()
 
 
-while fin == "False":
-    menu(input("""
+while not gfin:
+    gfin = menu(input("""
         elije opcion:
             1 - ver contactos
             2 - insertar contacto
